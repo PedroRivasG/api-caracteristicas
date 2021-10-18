b90fb7ed78361b63e1115c5b7f9fa28b30e2ece9
PedroRivas <pedroguardado96@gmail.com>
2021-10-17 19:26:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
+app.get('/', (req, res) => res.send("Hola mundo API de caracteristicas del comercio"));
+
+
+
+app.get('/caracteristicas', (req, res) => {
+    const caracteristicas = [
+
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "NO",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "NO",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "NO",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "NO",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        },
+        {
+            PetFrendly: "SI",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "SI",
+            UsoObligatorioDeMascarilla: "NO",
+            Delivery: "SI"
+        },
+        {
+            PetFrendly: "NO",
+            ProtocoloCOVId: "SI",
+            Baños: "SI",
+            AceptanBitcoin: "NO",
+            UsoObligatorioDeMascarilla: "SI",
+            Delivery: "NO"
+        }
+    ];
+    res.json(caracteristicas);
+});
