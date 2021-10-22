edb6a9c460de81180b16e919c38cc336eb3e0328
PedroRivas <pedroguardado96@gmail.com>
2021-10-21 22:08:33 -0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.get('/', (req, res) => res.send("Hola mundo API de caracteristicas del comercio"));
 
 app.get('/caracteristicas', (req, res) => {
